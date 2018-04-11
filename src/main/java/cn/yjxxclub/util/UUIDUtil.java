757c9d0e51c4b89a7f3ad190be01f28a2942dd37
tengxing <tengxing7452@163.com>
2018-04-11 17:57:02 +0800

    UUID

---
+++ src/main/java/cn/yjxxclub/util/UUIDUtil.java
@@ -0,0 +1,24 @@
+package cn.yjxxclub.util;
+
+import java.util.UUID;
+
+/**
+ * Author: Starry.Teng
+ * Email: tengxing7452@163.com
+ * Date: 17-10-5
+ * Time: 上午11:06
+ * Describe:UUID 工具类
+ */
+public class UUIDUtil {
+
+    /**
+     * 获取UUID
+     * @return
+     */
+    public static String getUUID(){
+        return UUID.randomUUID().toString().replace("-","");
+    }
+    public static void main(String[] args){
+        System.out.println(getUUID());
+    }
+}
